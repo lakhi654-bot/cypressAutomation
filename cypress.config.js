602faebf11247e1ea225ce6b93e67679fb62b40f
lakhi654 <lakshmi.654@gmail.com>
2025-02-10 10:36:22 -0600

    changes

--- cypress.config.js
+++ cypress.config.js
@@ -1,4 +1,24 @@
 const { defineConfig } = require("cypress");
+// const browserify = require("@cypress/browserify-preprocessor");
+// const {
+//   addCucumberPreprocessorPlugin,
+// } = require("@badeball/cypress-cucumber-preprocessor");
+// const {
+//   preprendTransformerToOptions,
+// } = require("@badeball/cypress-cucumber-preprocessor/browserify");
+ 
+//  async function setupNodeEvents(on,config)
+//  {
+//   await addCucumberPreprocessorPlugin(on, config);
+ 
+//   on(
+//     "file:preprocessor",
+//     browserify(preprendTransformerToOptions(config, browserify.defaultOptions)),
+//   );
+ 
+//   // Make sure to return the config object as it might have been modified by the plugin.
+//   return config;
+//  }
 const { beforeRunHook, afterRunHook } = require('cypress-mochawesome-reporter/lib');
 module.exports = defineConfig({
   defaultCommandTimeout:6000,
